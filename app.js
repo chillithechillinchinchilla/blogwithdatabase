e06446d8c559372d9bdc7089d78860e5637d7e84
Michael Grimsley <24793858+chillithechillinchinchilla@users.noreply.github.com>
2022-05-15 18:07:54 -0400

    Add mongo DB for posts, render on homepage

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
+// Set local db connection
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+// Set db Schema
+const postsSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  createdAt: {type: Date, default: Date.now}
+});
+//create the model
+const Post = mongoose.model('Post', postsSchema);
+
 
 app.set('view engine', 'ejs');
 
@@ -18,12 +32,15 @@ app.use(express.static("public"));
 
 let posts = [];
 
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
-});
+// We find all the posts, then sen to ejs page to render each value.
+app.get("/", function(req,res){
+  Post.find({}, function(err, foundPosts){
+     res.render("home", {
+       startingContent: homeStartingContent,
+       posts: foundPosts
+     }) // Render the home ejs page, with this content
+  }); // End Post.find
+}); // End app.get
 
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
@@ -37,17 +54,17 @@ app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = {
+// Post new blog conent to mongoDB
+app.post("/compose", function(req,res){
+  Post.create({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-
-  posts.push(post);
+  });
 
   res.redirect("/");
-
 });
+
+
 
 app.get("/posts/:postName", function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
